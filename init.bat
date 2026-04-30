9387e0c742305f3f170d92a633cc6a90fc2c204a
Dan Adrian Mirea <damirea84@gmail.com>
2026-04-30 14:50:21 +0300

    delete init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# raylib_phys_test" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/raylib_phys_test.git
-git push -u origin main
